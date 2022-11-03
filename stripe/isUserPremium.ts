21d565ac8efb310c786fdc25c71cb3fb8db1688b
Juan Brizuela <juanbrizuelago@gmail.com>
2022-11-03 12:22:46 -0600

    fixed issue

--- stripe/isUserPremium.ts
+++ stripe/isUserPremium.ts
@@ -8,7 +8,7 @@ export default async function isUserPremium(): Promise<boolean> {
 
   const querySnapshot = await firebase.firestore().collection('users').doc(uid).collection("payments").limit(1).get() 
 
-  const userDoc = await querySnapshot.empty ? false : querySnapshot.docs[0].data();
+  const userDoc = await !querySnapshot.empty && querySnapshot.docs[0].data();
 
   return userDoc?.status === "succeeded" ? true : false;
 }
