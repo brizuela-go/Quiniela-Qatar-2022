aec62e55b8fbd0bb330d9f1431d03d4a926ff2dd
Juan Brizuela <juanbrizuelago@gmail.com>
2022-11-10 10:12:25 -0600

    added loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,20 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { PremiumContext } from "../context/PremiumContext";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 500,
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const theme = createTheme({
   typography: {
